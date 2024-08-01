f5ff395aa3e6f90edd90e75e508dad26284bf6db
Iniyan S.V. <106759610+Iniyan2003@users.noreply.github.com>
2024-08-01 06:24:00 +0530

    Delete Functions directory
    Removed the unnecessary files.

--- Functions/src/main/java/com/Sunnxt/Functions/Repository/ContentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.Sunnxt.Functions.Repository;
-
-import com.Sunnxt.Functions.Entity.Content;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ContentRepository extends JpaRepository<Content, Integer> {
-    // Define custom query methods if needed
-}
--- Functions/src/main/java/com/Sunnxt/Functions/Repository/SubscriptionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.Sunnxt.Functions.Repository;
-
-import com.Sunnxt.Functions.Entity.Subscription;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SubscriptionRepository extends JpaRepository<Subscription, Integer> {
-    // Define custom query methods if needed
-}
